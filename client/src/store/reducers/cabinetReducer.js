30cfdc55eebaa1de7f19445719d65c51fba8d364
Dmitri <54492227+purpl3mint@users.noreply.github.com>
2022-05-19 13:25:55 +0700

    Added changing password

--- client/src/store/reducers/cabinetReducer.js
+++ client/src/store/reducers/cabinetReducer.js
@@ -1,9 +1,16 @@
 import {
-    CABINET_SET_LOGIN
-} from '../actions/cabinetActions'
+    CABINET_SET_LOGIN,
+    CABINET_SET_PASSWORD_FORM,
+    CABINET_CLEAR_PASSWORD_FORM
+  } from '../actions/cabinetActions'
   
 const initialState = {
-  login: 'Неизвестно'
+  login: 'Неизвестно',
+  setPasswordForm: {
+      id: 0,
+      password: '',
+      newPassword: ''
+  }
 }
   
 function cabinetReducer(state = initialState, action) {
@@ -11,7 +18,17 @@ function cabinetReducer(state = initialState, action) {
         case CABINET_SET_LOGIN: {
             return { ...state, login: action.data }
         }
-  
+        case CABINET_SET_PASSWORD_FORM: {
+            const newPasswordForm = { ...state.setPasswordForm, [action.data.name]: action.data.value}
+
+            return { ...state, setPasswordForm: newPasswordForm }
+        }
+
+        case CABINET_CLEAR_PASSWORD_FORM: {
+
+            return { ...state, setPasswordForm: initialState.setPasswordForm }
+        }
+
         default: {
             return state
         }
